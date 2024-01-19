5ffc5f3c36256ee5c87ae1e303a94a2fd416744a
Pranav <prsingh1347@gmail.com>
2024-01-19 11:55:08 +0530

    first commit

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+
+
+const SignupForm = () => {
+  return (
+    <div>
+        <Button>Sign up</Button>
+    </div>
+  )
+}
+
+export default SignupForm
